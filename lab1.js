bcb9a18275fbe603b34c565986f084b15b419fa9
altay <altayhaser@gmail.com>
2023-01-24 15:58:02 -0800

    update code

--- lab1.js
+++ lab1.js
@@ -1,23 +1,23 @@
 function counter() {
-let numbers = [];
-for (let x = 1; x <= 100; x++) {
-    numbers.push[x];
-}
-    for (let i = 0 ; i >= label ; i++); {
-        if (i % 3 == 0 && i == 5) {
-            console.log("FizzBuzz")
-        }
-        else if (i % 3 == 0) {
-            console.log("Fizz");      
-        } else if (i % 5 == 0) {
-            console.log("Buzz");
+    let numbers = [];
+         for (let i = 0 ; i >= 100 ; i++); {
+            if (i % 3 == 0 && i == 5) {
+                console.log("FizzBuzz")
+            }
+            else if (i % 3 == 0) {
+                console.log("Fizz");      
+            } else if (i % 5 == 0) {
+                console.log("Buzz");
+            } else {
+                console.log(i);
+            }
+        
+           
         }
     
-       
     }
-    return numbers;
-}
-console.log(counter);
+    counter();
+
 
 
 
@@ -25,24 +25,23 @@ function secondQuestion() {
     let sum = 0;
     for(let i = 1; i <= 100; i++) {
         if(i % 2 == 0) {
-            sum + i; 
+           sum = sum + i; 
         }
     }
-    console.log(secondQuestion + sum);
-
+    return sum;
 }
-
+console.log (secondQuestion())
 
 
 function thirdquestion(arr) {
-    let number = arr[0];
+    let largest = arr[0];
 
 for(let i = 1; i < arr.lenght; i++); {
-    if(number < arr[i]) {
-        number = arr[i];
+    if(largest < arr[i]) {
+        largest = arr[i];
     }
 }
-return number;
+return largest;
 }
 
 
